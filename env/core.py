3a54fe8ad6239d5390e8d09393009d362b3a295f
Bhagavatkumar <158759513+Bhagavatkumar@users.noreply.github.com>
2026-04-08 20:16:56 +0530

    Refactor grading logic in CustomerSupportEnv

--- env/core.py
+++ env/core.py
@@ -1,40 +1,39 @@
 from env.tasks import TASKS
-from env.grader import evaluate_response
+from env.grader import grade   # FIXED
 
 class CustomerSupportEnv:
     def __init__(self):
         self.current_task = None
         self.step_count = 0
+        self.history = []
 
     def reset(self):
         import random
         self.current_task = random.choice(TASKS)
         self.step_count = 0
+        self.history = []
 
         return {
             "ticket_id": self.current_task["id"],
             "message": self.current_task["message"],
-            "history": [],
+            "history": self.history,
             "step_count": self.step_count
         }
 
     def step(self, action):
         self.step_count += 1
 
-        response = action.get("content", "")
+        self.history.append(action)
 
-        reward = evaluate_response(
-            self.current_task["message"],
-            response,
-            self.current_task["solution_keywords"]
-        )
+        #  FIX: use grade()
+        reward = grade(self.current_task, self.history)
 
-        done = reward > 0
+        done = reward > 0.5
 
         state = {
             "ticket_id": self.current_task["id"],
             "message": self.current_task["message"],
-            "history": [response],
+            "history": self.history,
             "step_count": self.step_count
         }
 
