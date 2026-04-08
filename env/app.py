170888972a270d9718662037112fdb6e3755c004
Bhagavatkumar <158759513+Bhagavatkumar@users.noreply.github.com>
2026-04-08 15:16:55 +0530

    Add files via upload

---
+++ env/app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from env.core import CustomerSupportEnv
+
+app = FastAPI()
+env = CustomerSupportEnv()
+
+@app.get("/")
+def home():
+    return {"message": "CSRE running"}
+
+@app.get("/reset")
+def reset():
+    return env.reset()
+
+@app.get("/state")
+def state():
+    return env.state()
+
+@app.post("/step")
+def step(action: dict):
+    return env.step(action)
